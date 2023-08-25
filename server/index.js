65dea2d963f4527cf8be9b008c460cf9c8388a8b
NaveedMaq <naveedmaqbool30@gmail.com>
2023-08-25 10:07:24 +0530

    test: Change home route to json

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=> {
-  res.send('<h1>Todo Api</h1>')
+  res.json({message: "test modified"})
 })
 
 app.use("/admin", adminRouter)
