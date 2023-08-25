6b60aee1c92cfe892559b3ebd97b5a74dc9cac4d
NaveedMaq <naveedmaqbool30@gmail.com>
2023-08-25 10:11:44 +0530

    test: Modified home route 2

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=> {
-  res.json({message: "test modified"})
+  res.json({message: "222222222"})
 })
 
 app.use("/admin", adminRouter)
